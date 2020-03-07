5d5a52e12ad644264ce3310f676970dacbbca4f4
Lukas Murch <54785431+gftradco@users.noreply.github.com>
2020-03-07 13:35:48 -0500

    Update CryptoNoteConfig.h

--- src/config/CryptoNoteConfig.h
+++ src/config/CryptoNoteConfig.h
@@ -1,6 +1,7 @@
 // Copyright (c) 2012-2017, The CryptoNote developers, The Bytecoin developers
 // Copyright (c) 2014-2018, The Monero Project
 // Copyright (c) 2018-2019, The TurtleCoin Developers
+// Copyright (c) 2020, The Anacas Steel Developers
 //
 // Please see the included LICENSE file for more information.
 
@@ -298,7 +299,7 @@ namespace CryptoNote
         const char MINER_CONFIG_FILE_NAME[] = "miner_conf.json";
     } // namespace parameters
 
-    const char CRYPTONOTE_NAME[] = "TurtleCoin";
+    const char CRYPTONOTE_NAME[] = "Anacas";
 
     const uint8_t TRANSACTION_VERSION_1 = 1;
 
@@ -379,16 +380,11 @@ namespace CryptoNote
     const std::string LICENSE_URL = "https://github.com/turtlecoin/turtlecoin/blob/master/LICENSE";
 
     const static boost::uuids::uuid CRYPTONOTE_NETWORK = {
-        {0xb5, 0x0c, 0x4a, 0x6c, 0xcf, 0x52, 0x57, 0x41, 0x65, 0xf9, 0x91, 0xa4, 0xb6, 0xc1, 0x43, 0xe9}};
+        {0xa1, 0x0c, 0x4a, 0x6c, 0x1af, 0x52, 0x57, 0x41, 0x4a, 0xf9, 0x91, 0xa4, 0xb6, 0xa1, 0x43, 0xe9}};
 
     const char *const SEED_NODES[] = {
-        "206.189.142.142:11897", // rock
-        "145.239.88.119:11999", // cision
-        "142.44.242.106:11897", // tom
-        "165.227.252.132:11897", // iburnmycd
-        "148.251.178.238:11897", // hv
-        "45.32.138.7:11897", // extra
-        "46.214.70.196:11897", // CuveeRO
-        "94.113.119.122:11897" // CuveeCZ
+        "000.000.000.000:211269", // node one
+        "000.000.00.000:211269", // node two
+        
     };
 } // namespace CryptoNote
