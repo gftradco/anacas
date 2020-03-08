df8e60f631bb37ab0b009a2ce049799737e5e6b3
Lukas Murch <54785431+gftradco@users.noreply.github.com>
2020-03-08 11:32:24 -0400

    Update CryptoNoteConfig.h

--- src/config/CryptoNoteConfig.h
+++ src/config/CryptoNoteConfig.h
@@ -384,7 +384,7 @@ namespace CryptoNote
 
     const char *const SEED_NODES[] = {
         "45.32.168.228:211269", // node one
-        "000.000.00.000:211269", // node two
+        
         
     };
 } // namespace CryptoNote
